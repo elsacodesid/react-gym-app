c9f2800aae76b1f9b1605fc4229f2825a2b143cd
elsacodesid <elsacodesid@gmail.com>
2024-06-29 11:10:09 +0700

    update

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
+import React from "react";
 
-const SectionWrapper = ({children, header, title}) => {
+const SectionWrapper = ({ children, header, title }) => {
   return (
     <section className="min-h-screen flex flex-col gap-10">
       <div className="bg-slate-950 py-10 flex flex-col gap-2 justify-center items-center">
-        <p className="uppercase font-medium">
-          {header}
-        </p>
-        <h2>{title[0]} <span>{title[1]}</span> {title[2]}</h2>
+        <p className="uppercase font-medium">{header}</p>
+        <h2>
+          {title[0]} <span className="uppercase text-blue-400">{title[1]}</span>{" "}
+          {title[2]}
+        </h2>
       </div>
       {children}
     </section>
-  )
-}
+  );
+};
 
-export default SectionWrapper
+export default SectionWrapper;
