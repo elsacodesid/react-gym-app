1812a87b63d81e706b108b7ceaadb56927886963
elsacodesid <elsacodesid@gmail.com>
2024-07-01 09:17:11 +0700

    update

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import SectionWrapper from "./SectionWrapper";
 import { WORKOUTS } from "../utils/exercises";
 
 const Generator = () => {
+  const [showModal, setShowModal] = useState(false);
+  function toggleModal() {
+    setShowModal(!showModal);
+  }
   function Header({ index, title, description }) {
     return (
       <div className="flex flex-col gap-4">
         <div className="flex items-center justify-center gap-2">
-          <p className="text-3xl sm:text-4xl md:text-5xl font-semibold text-slate-400">{index}</p>
+          <p className="text-3xl sm:text-4xl md:text-5xl font-semibold text-slate-400">
+            {index}
+          </p>
           <h4 className="text-xl sm:text-2xl md:text-3xl">{title}</h4>
         </div>
         <p className="text-sm sm:text-base mx-auto">{description}</p>
@@ -19,18 +25,39 @@ const Generator = () => {
       header={"generate your workout"}
       title={["it's", "Fit", "O'Clock"]}
     >
-      <Header index={"01"} title={"Pick your poison"} description={"Select the workout you wish to endure"} />
+      <Header
+        index={"01"}
+        title={"Pick your poison"}
+        description={"Select the workout you wish to endure"}
+      />
       <div className="grid grid-cols-2 sm:grid-cols-4 gap-4">
-         {Object.keys(WORKOUTS).map((type, typeIndex) => {
-        return (
-          <button className="bg-slate-950 border border-blue-400 py-3 rounded-lg duration-200 hover:border-blue-600" key={typeIndex}><p>{type}</p></button> 
-        )
-      })}
+        {Object.keys(WORKOUTS).map((type, typeIndex) => {
+          return (
+            <button
+       
+              className="bg-slate-950 border border-blue-400 py-3 rounded-lg duration-200 hover:border-blue-600"
+              key={typeIndex}
+            >
+              <p className="capitalize">{type.replaceAll("_", " ")}</p>
+            </button>
+          );
+        })}
       </div>
-  
-      
+
+      <Header
+        index={"02"}
+        title={"Lock on targets"}
+        description={"Select the muscles judged for annihilation."}
+      />
+      <div className="bg-slate-950 py-3 border border-solid border-blue-400 rounded-lg flex flex-col">
+        <button onClick={toggleModal} className="relative  flex items-center justify-center">
+          <p>Select muscle groups</p>
+          <i className="fa-solid fa-caret-down p-3 absolute right-3 top-1/2 -translate-y-1/2"></i>
+        </button>
+        {showModal && <div>modal</div>}
+      </div>
     </SectionWrapper>
-  );  
+  );
 };
 
 export default Generator;
